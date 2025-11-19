bab2c8ac83089dbeefac1a5e19c34dca27992ef9
OneTrueHomie <vicigwilo@gmail.com>
2025-11-19 22:30:48 +0100

    Update Posm.sol exercise

--- foundry/src/exercises/Posm.sol
+++ foundry/src/exercises/Posm.sol
@@ -17,9 +17,10 @@ contract PosmExercises {
     // currency0 = ETH for this exercise
     constructor(address currency1) {
         IERC20(currency1).approve(PERMIT2, type(uint256).max);
-        IPermit2(PERMIT2).approve(
-            currency1, address(posm), type(uint160).max, type(uint48).max
-        );
+        IPermit2(PERMIT2)
+            .approve(
+                currency1, address(posm), type(uint160).max, type(uint48).max
+            );
     }
 
     receive() external payable {}
@@ -89,8 +90,7 @@ contract PosmExercises {
     }
 
     function burn(uint256 tokenId, uint128 amount0Min, uint128 amount1Min)
-        external
-    {
+        external {
         // Write your code here
     }
 }
